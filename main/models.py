4e3ce03b1c87094437b36c6002939e9fbd064140
kasia.kordys <kasia.kordys@gmail.com>
2021-04-20 22:44:43 +0100

    add models and register them in admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+from autoslug import AutoSlugField
+
+class Topic(models.Model):
+	title = models.CharField(max_length=200)
+	slug = AutoSlugField(populate_from = 'title')
+
+	def __str__(self):
+		return self.title
 # Create your models here.
+class Recipe(models.Model):
+	title = models.CharField(max_length=200)
+	slug = AutoSlugField(populate_from = 'title')
+	image = models.CharField(max_length=400)
+	description = models.TextField()
+	ingredients = models.TextField()
+	directions = models.TextField()
+	servings = models.CharField(max_length=5)
+	time = models.CharField(max_length=5)
+	calories = models.CharField(max_length=5)
+	fat = models.CharField(max_length=5)
+	carbs = models.CharField(max_length=5)
+	protein = models.CharField(max_length=5)
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
