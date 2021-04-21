861130c96730510a791821f72b84ad260b362bea
kasia.kordys <kasia.kordys@gmail.com>
2021-04-21 22:42:57 +0100

    add search method def

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, search
+
 
 # url for the home page first, create a view for it 
 urlpatterns = [
 	path('', home, name='home'),
+	path('search', search, name='search'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Recipe
+from django.db.models import Q
 # Create your views here.
 def home(request):
 	total_recipes = Recipe.objects.all().count()
@@ -8,3 +9,23 @@ def home(request):
 	'total_recipes': total_recipes,
 	}
 	return render(request, 'home.html', context)
+
+
+def search(request):
+	recipes = Recipe.objects.all()
+
+	if 'search' in request.GET:
+		# we need to get the object the user is searching for
+		query = request.GET.get('search')
+		# filter recipes by title that contains query
+		queryset = recipes.filter(Q(title__icontains=query))
+
+	if request.GET.get('breakfast'):
+		# filter results by category selected by user
+		result = queryset.filter(Q(topic__title__icontains='breakfast'))
+
+		print('\n\n\n')
+		print(result)
+
+	context = {}
+	return render(request, 'search.html', context)
